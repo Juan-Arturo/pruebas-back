b53282d75f46e849e75890e56d98069504753005
Luis angel <luisangelsanchezsalazar1@gmail.com>
2025-04-25 18:42:24 -0600

    Ya carga la informacion del diagnostico pueden revisarlo

--- routes/aneec/evaluator.route.ts
+++ routes/aneec/evaluator.route.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import {
-    accesoController
-
+    accesoController,
+    registrarDiagnostico,
+    uploadDiagnostico
 } from "../../controllers/aneec/evaluator.controller";
 import { validateRequest } from "../../middlewares/validateRequest.md";
-
 import { validateJwt } from "../../middlewares/validate.md";
 
 const router = Router();
 
-router.get("/pruebaConexion",accesoController)
+router.get("/pruebaConexion", accesoController)
+
+router.post("/registrarDiagnostico", uploadDiagnostico, registrarDiagnostico)
 
 export default router;
